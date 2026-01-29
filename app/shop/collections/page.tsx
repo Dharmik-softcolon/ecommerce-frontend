9ac9fc678bb743ea9bd7a169c472730a2b5e86b1
Dharmik G <dharmik.softcolon@gmail.com>
2026-01-29 16:19:27 +0530

    code issue resolved successfully

---
+++ app/shop/collections/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function ShopCollectionsPage() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.replace('/collections');
+    }, [router]);
+
+    return null;
+}
---
+++ app/shop/products/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function ShopProductsPage() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.replace('/collections');
+    }, [router]);
+
+    return null;
+}
